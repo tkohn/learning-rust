10193990e904379cf9e378720f9f9b56b83260fa
Torsten Kohn <tkohn@users.noreply.github.com>
2023-08-25 08:40:35 +0200

    Chapter 5.3 Method Syntax
    Add the following methods to the struct:
    - area: example for a method in a struct with the self argument
    - can_hold: example for a method in a struct with self and additional parameters
    
    Add square function to the struct without the self parameter. Used to create a Rectangle where width and height are equal.

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,20 +4,49 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        (self.width > other.width && self.height > other.height)
+            || (self.height > other.width && self.width > other.height)
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 10,
+        height: 45,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    let square1 = Rectangle::square(25);
+
     println!("rect1 is {:?}", rect1);
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-}
+    println!("The area of rect1 is {} square pixels.", rect1.area());
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!(
+        "Can rect1 hold rect3? {}",
+        Rectangle::can_hold(&rect1, &rect3)
+    );
+    println!("Can rect1 hold square1? {}", rect1.can_hold(&square1));
 }
