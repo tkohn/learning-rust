71229821cfb8744363f044a29521b5ad81b82a19
Torsten Kohn <tkohn@users.noreply.github.com>
2023-08-25 10:58:02 +0200

    Chapter 5.3 Method Syntax
    Add examples for Read, Write and Owner permissions:
    
    - Add method set_to_max and Copy, Clone trait to Rectangle as Example
      for changing Permissions for the method max.

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -31,6 +31,14 @@ impl Rectangle {
             height: self.height.max(other.height),
         }
     }
+    // for compiling this method Copy and Clone trait is needed
+    // with the trait the method 'fn max(self, other: Rectangle) -> Rectangle'
+    // does not need Owner permission for self and other.
+    // Important Note: This works because the struct does not use any Heap Memory,
+    // adding a String property to Rectangle would result into a compilation error!
+    fn set_to_max(&mut self, other: Rectangle) {
+        *self = self.max(other);
+    }
     // need write permission, you can only use it when the created Rectangle is mutable
     fn set_width(&mut self, width: u32) {
         self.width = width;
@@ -38,7 +46,7 @@ impl Rectangle {
 }
 
 fn main() {
-    let rect1 = Rectangle {
+    let mut rect1 = Rectangle {
         width: 30,
         height: 50,
     };
@@ -53,12 +61,14 @@ fn main() {
         height: 45,
     };
 
-    let square1 = Rectangle::square(25);
+    // mutable to test square1.set_to_max(rect2);
+    let mut square1 = Rectangle::square(25);
 
     println!("rect1 is {:?}", rect1);
 
     println!("The area of rect1 is {} square pixels.", rect1.area());
 
+    rect1.set_width(31);
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!(
         "Can rect1 hold rect3? {}",
@@ -68,4 +78,8 @@ fn main() {
 
     println!("rect1.max(rect3): {:?}", rect1.max(rect3));
     // rect1 and rect3 can not be used anymore
+
+    println!("{:?}", square1);
+    square1.set_to_max(rect2);
+    println!("{:?}", square1);
 }
