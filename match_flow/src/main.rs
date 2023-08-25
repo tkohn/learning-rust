ded044288b3a39c19a2264c52bd2d55e0d24148a
Torsten Kohn <tkohn@users.noreply.github.com>
2023-08-25 13:43:04 +0200

    Chapter 6.2 The match Control Flow Construct
    Add example for Option type and match control flow

--- match_flow/src/main.rs
+++ match_flow/src/main.rs
@@ -29,4 +29,14 @@ fn value_in_cents(coin: Coin) -> u8 {
 fn main() {
     println!("Value: {}", value_in_cents(Coin::Penny));
     println!("Value: {}", value_in_cents(Coin::Quarter(UsState::Alabama)));
+
+    println!("{:?}", plus_one(Some(10)));
+    println!("{:?}", plus_one(None));
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
